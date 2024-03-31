10efd2a8bf44187d4ceef7d489a28914b1863f6f
Minor Gordon <minorg@server.fake>
2024-03-31 11:42:39 -0400

    cleaning up a few files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [],
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +15,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 };
 export default config;
