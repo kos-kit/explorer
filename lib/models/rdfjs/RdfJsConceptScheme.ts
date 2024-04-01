fe85391c1e38f405e5025ad00f4e7ce7347d1208
Minor Gordon <minorg@server.fake>
2024-03-31 20:37:03 -0400

    working on concept scheme

--- lib/models/rdfjs/RdfJsConceptScheme.ts
+++ lib/models/rdfjs/RdfJsConceptScheme.ts
@@ -4,6 +4,8 @@ import { ConceptScheme } from "../ConceptScheme";
 import { RdfJsLabeledModel } from "./RdfJsLabeledModel";
 import TermSet from "@rdfjs/term-set";
 import { skos } from "@/lib/vocabularies";
+import { RdfJsConcept } from "./RdfJsConcept";
+import invariant from "ts-invariant";
 
 export class RdfJsConceptScheme
   extends RdfJsLabeledModel
@@ -12,14 +14,31 @@ export class RdfJsConceptScheme
   *concepts(): Iterable<Concept> {
     const conceptIdentifierSet = new TermSet<BlankNode | NamedNode>();
 
+    // ConceptScheme -> Concept statement
     yield* this.filterAndMapObjects(skos.hasTopConcept, (term) => {
       switch (term.termType) {
         case "BlankNode":
         case "NamedNode":
-          return new RdfJsConcept(this.dataset, term);
-        default:
-          return null;
+          if (!conceptIdentifierSet.has(term)) {
+            conceptIdentifierSet.add(term);
+            return new RdfJsConcept(this.dataset, term);
+          }
       }
+      return null;
     });
+
+    // Concept -> ConceptScheme statement
+    for (const predicate of [skos.inScheme, skos.topConceptOf]) {
+      for (const quad of this.dataset.match(null, predicate, this.identifier)) {
+        invariant(
+          quad.subject.termType === "BlankNode" ||
+            quad.subject.termType === "NamedNode",
+        );
+        if (!conceptIdentifierSet.has(quad.subject)) {
+          conceptIdentifierSet.add(quad.subject);
+          yield new RdfJsConcept(this.dataset, quad.subject);
+        }
+      }
+    }
   }
 }
