6ff99a056c2ef2ece5f1431c69cb8708e16d3527
Minor Gordon <minorg@server.fake>
2024-03-31 20:05:24 -0400

    switch jest.config back to .js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+const nextJest = require("next/jest");
 
+/** @type {import('jest').Config} */
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
 // Add any custom config to be passed to Jest
-const config: Config = {
+const config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   testMatch: ["**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)"],
@@ -15,5 +15,5 @@ const config: Config = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
 
-// Adapted from https://github.com/vercel/next.js/issues/40183
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(config);
