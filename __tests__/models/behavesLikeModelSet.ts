a2560e8add1a3e00642bf68acc43d4290e5cc3fd
Minor Gordon <minorg@server.fake>
2024-04-04 19:11:49 -0400

    new ModelSet methods and failing tests

--- __tests__/models/behavesLikeModelSet.ts
+++ __tests__/models/behavesLikeModelSet.ts
@@ -1,8 +1,51 @@
 import { ModelSet } from "@/lib/models/ModelSet";
 
 export const behavesLikeModelSet = (modelSet: ModelSet) => {
+  it("should get concepts", () => {
+    const firstConcepts = [...modelSet.concepts({ limit: 10, offset: 0 })];
+    expect(firstConcepts).toHaveLength(10);
+
+    const nextConcepts = [...modelSet.concepts({ limit: 10, offset: 10 })];
+    expect(nextConcepts).toHaveLength(10);
+    for (const nextConcept of nextConcepts) {
+      expect(
+        firstConcepts.every(
+          (firstConcept) =>
+            !firstConcept.identifier.equals(nextConcept.identifier),
+        ),
+      ).toBeTruthy();
+    }
+  });
+
+  it("should get a concept by its identifier", () => {
+    for (const concept of modelSet.concepts({ limit: 1, offset: 0 })) {
+      expect(
+        concept.identifier.equals(
+          modelSet.conceptByIdentifier(concept.identifier).identifier,
+        ),
+      ).toBeTruthy();
+      return;
+    }
+    fail();
+  });
+
+  it("should get a count of concepts", () => {
+    expect(modelSet.conceptsCount).toBeGreaterThan(0);
+  });
+
   it("should get concept schemes", () => {
     expect(modelSet.conceptSchemes).toHaveLength(1);
+  });
+
+  it("should get a concept scheme by an identifier", () => {
+    for (const conceptScheme of modelSet.conceptSchemes) {
+      expect(
+        conceptScheme.identifier.equals(
+          modelSet.conceptSchemeByIdentifier(conceptScheme.identifier)
+            .identifier,
+        ),
+      ).toBeTruthy();
+    }
   });
 
   it("should get language tags", () => {
