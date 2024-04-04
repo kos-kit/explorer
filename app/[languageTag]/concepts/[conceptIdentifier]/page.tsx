8b6cc0da9254f975a04191339207bc421b9b4239
Minor Gordon <minorg@server.fake>
2024-04-04 19:00:11 -0400

    start of ConceptPage

---
+++ app/[languageTag]/concepts/[conceptIdentifier]/page.tsx
@@ -0,0 +1,55 @@
+import modelSet from "@/app/modelSet";
+import { Pages } from "@/lib/Pages";
+import { LanguageTag } from "@/lib/models/LanguageTag";
+import { defilenamify } from "@/lib/utilities/defilenamify";
+import { filenamify } from "@/lib/utilities/filenamify";
+import { identifierToString } from "@/lib/utilities/identifierToString";
+import { stringToIdentifier } from "@/lib/utilities/stringToIdentifier";
+import { Metadata } from "next";
+
+interface ConceptPageParams {
+  conceptIdentifier: string;
+  languageTag: LanguageTag;
+}
+
+export default function ConceptPage({
+  params: { conceptIdentifier, languageTag },
+}: {
+  params: ConceptPageParams;
+}) {
+  const concept = modelSet.conceptByIdentifier(
+    stringToIdentifier(defilenamify(conceptIdentifier)),
+  );
+
+  return <ConceptPageComponent concept={concept} languageTag={languageTag} />;
+}
+
+export function generateMetadata({
+  params: { conceptIdentifier, languageTag },
+}: {
+  params: ConceptPageParams;
+}): Metadata {
+  const concept = modelSet.conceptByIdentifier(
+    stringToIdentifier(defilenamify(conceptIdentifier)),
+  );
+
+  return Pages.concept({ concept, languageTag }).metadata;
+}
+
+export function generateStaticParams(): ConceptPageParams[] {
+  const staticParams: ConceptPageParams[] = [];
+
+  const concepts = modelSet.concepts;
+  if (concepts.length > 1) {
+    for (const concept of concepts) {
+      for (const languageTag of modelSet.languageTags) {
+        staticParams.push({
+          conceptIdentifier: filenamify(identifierToString(concept.identifier)),
+          languageTag,
+        });
+      }
+    }
+  } // Else the root page will be the concept scheme
+
+  return staticParams;
+}
