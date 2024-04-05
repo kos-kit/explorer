da4b4f35b26e733fb1f0d08e60e97c4b91535d7b
Minor Gordon <minorg@server.fake>
2024-04-05 15:31:16 -0400

    Model: add DC accessors

--- lib/models/Model.ts
+++ lib/models/Model.ts
@@ -1,1 +1,9 @@
-export interface Model {}
+import { Literal, NamedNode } from "@rdfjs/types";
+import { LanguageTag } from "./LanguageTag";
+
+export interface Model {
+  license(languageTag: LanguageTag): Literal | NamedNode | null;
+  readonly modified: Literal | null;
+  rights(languageTag: LanguageTag): Literal | null;
+  rightsHolder(languageTag: LanguageTag): Literal | null;
+}
