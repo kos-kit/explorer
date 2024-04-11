1473b1d3a6c90a03d86c63806362cfa429c459e6
Minor Gordon <minorg@server.fake>
2024-04-11 11:50:26 -0400

    appease linter

--- lib/components/PageTitleHeading.tsx
+++ lib/components/PageTitleHeading.tsx
@@ -1,3 +1,5 @@
-export function PageTitleHeading({ children }: PropsWithChildren<{}>) {
+import { PropsWithChildren } from "react";
+
+export function PageTitleHeading({ children }: PropsWithChildren) {
   return <h1 className="font-bold text-xl">{children}</h1>;
 }
