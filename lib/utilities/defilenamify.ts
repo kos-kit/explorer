3310c599e9e69d89b988826dece83c15875e88aa
Minor Gordon <minorg@server.fake>
2024-05-29 12:38:33 -0400

    move code to client

--- lib/utilities/defilenamify.ts
+++
@@ -1,4 +0,0 @@
-import { base58 } from "@/lib/utilities/base58";
-
-export const defilenamify = (value: string): string =>
-  base58.decode(value).toString("utf-8");
--- lib/utilities/filenamify.ts
+++
@@ -1,4 +0,0 @@
-import { base58 } from "@/lib/utilities/base58";
-
-export const filenamify = (value: string): string =>
-  base58.encode(Buffer.from(value, "utf-8"));
