ede69d40ba7efbcf441e706c8f8a44e75a3ac94a
Minor Gordon <minorg@server.fake>
2024-05-30 13:06:18 -0400

    fix configuration default

--- app/configuration.ts
+++ app/configuration.ts
@@ -48,7 +48,7 @@ if (!configuration.value) {
       default: ".kos-kit/explorer/cache",
     }),
     INPUT_CONCEPTS_PER_PAGE: intValidator({ default: 25 }),
-    INPUT_DATA_PATHS: filePathArrayValidator({ default: "" }),
+    INPUT_DATA_PATHS: filePathArrayValidator({ default: [] }),
     INPUT_DEFAULT_LANGUAGE_TAG: envalid.str({ default: "en" }),
     INPUT_NEXT_BASE_PATH: envalid.str({ default: "" }),
     INPUT_NEXT_OUTPUT: envalid.str({ default: "" }),
